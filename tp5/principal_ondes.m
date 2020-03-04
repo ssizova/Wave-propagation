caf2df5e15c59aef6774aba3742608bdacbb5ef4
ssizova <sizova96@mail.ru>
2020-03-04 23:37:21 +0100

    main function

--- tp5/principal_ondes.m
+++ tp5/principal_ondes.m
@@ -1,4 +1,4 @@
-function principal_ondes(maillages,h,T, type_MM)
+function principal_ondes(maillages,h,T, type_MM, representation)
 % =====================================================
 %
 % principal_ondes(nom_maillage,T);
@@ -20,7 +20,6 @@ fprintf('Eq. des ondes homogene (Elts Finis P1 ; schema saute-mouton)\n');
 % -----------------
 % Vitesse de propagation 
 c = 2 ;
-
 for h_index = 1:size(h)
   h_i = h(h_index);
   t = cputime;
@@ -42,94 +41,100 @@ for h_index = 1:size(h)
     % calcul des matrices elementaires du triangle l 
     
   % calcul des matrices elementaires (rigidite et masse) 
-    Kel=matK_elem(S1, S2, S3, Reftri(l));
-    Mel=matM_elem(S1, S2, S3);
+      Kel=matK_elem(S1, S2, S3, Reftri(l));
+      Mel=matM_elem(S1, S2, S3);
     
-  if (type_MM == 0)
-    for i=1:3
-    I = Numtri(l,i);
-      for j=1:3
-        J = Numtri(l,j);
-        MM(I,J) = MM(I,J) + Mel(i,j);
-      end
-    end
-  end
-  
+      if (type_MM == 0)
+        for i=1:3
+        I = Numtri(l,i);
+          for j=1:3
+            J = Numtri(l,j);
+            MM(I,J) = MM(I,J) + Mel(i,j);
+          end
+        end
+      end 
   % On fait l'assemblage des matrices et du second membre
-  for i=1:3
-    I = Numtri(l,i);
-       for j=1:3
-          J = Numtri(l,j);
-          KK(I,J)=KK(I,J)+Kel(i,j);
-       end
-   end
-end % for l
+      for i=1:3
+        I = Numtri(l,i);
+           for j=1:3
+              J = Numtri(l,j);
+              KK(I,J)=KK(I,J)+Kel(i,j);
+           end
+      end
+    end % for l
+  % Calcul de la CFL
+  if(type_MM == 0)
+  lambda = eigs(KK,MM);
+  lambda_max = lambda(1);
+  else
+  k = 1/h_i/h_i;
+  lambda_max = eigs(k*KK,1);
+  end
+  delta_t = sqrt(4/lambda_max);
+  fprintf('Temps final %6.2f s ; Pas de temps (CFL) %10.6f s\n',T,delta_t);
+  t_end = cputime - t
 
-% Calcul de la CFL
-if(type_MM == 0)
-lambda = eigs(KK,MM);
-lambda_max = lambda(1);
-else
-k = 1/h_i/h_i;
-lambda_max = eigs(k*KK,1);
-end
-delta_t = sqrt(4/lambda_max);
-fprintf('Temps final %6.2f s ; Pas de temps (CFL) %10.6f s\n',T,delta_t);
-t_end = cputime - t
+
+  % Nombre de pas de temps
+  % ----------------------
+  Nb_temps = round(T/delta_t);
+
+  % Debut du chronometre
+  cptime = cputime;
+
+  % solution a t=0 (U0(x) = 0 et U1(x) = 0)
+  Minv = inv(MM);
+  F0 = MM * f_gauss(Coorneu(1:Nbpt,1),Coorneu(1:Nbpt,2),0);
+  U0 = zeros(Nbpt,1);
+  U1 = zeros(Nbpt,1);
+  UU0 = U0;
+  MUU1 = UU0 - c*c * delta_t^2/2 * KK * UU0+delta_t^2/2 * F0 + delta_t * U1;
+  ##UU1 =  MUU1;
+  ##UU1 = UU0 -  c^2 * delta_t^2/2*MM\KK*UU0+delta_t^2/2*F0;
+  UU1 = MM\MUU1;
+  % Boucle sur les pas de temps
+  % ---------------------------
+  % Uk-1 = UU0, Uk = UU1 et Uk+1 = UU2
+  for k = 1:Nb_temps
+    
+      tk = k*delta_t;
+          
+      % Calcul du second membre a l'instant tk
+      % --------------------------------------
+      % 1. Contribution de la source
+      tilde_FF = f_gauss(Coorneu(1:Nbpt,1),Coorneu(1:Nbpt,2),tk);
+
+      % 2. Contribution des iteres precedents
+      % .
+      % A COMPLETER
+      % .
+
+      % Obtention de Uk+1
+      % -----------------
+  ##    UU2 = inv(MM)*(delta_t^2 * tilde_FF - c^2 * delta_t^2*KK*UU1 +2*UU1 - UU0);
+      UU2 = (tilde_FF- c * c * MM\KK*UU1)*delta_t^2 + 2*UU1-UU0;
+
+      % Visualisation 
+      % -------------
+      if representation
+
+        affiche(UU2, Numtri, Coorneu, ['Temps = ', num2str(tk)]);
+        axis([min(Coorneu(:,1)),max(Coorneu(:,1)),min(Coorneu(:,2)),...
+              max(Coorneu(:,2)),-0.0002 0.0003  -0.00001 0.00015]);
+       endif  
+      % Mise a jour des iteres
+      % ----------------------
+      UU0 = UU1;
+      UU1 = UU2;    
+  end
+  % Arret du chronometre
+  % --------------------
+  % A l'aide de cputime
+  temps_calcul = cputime - cptime;
+  cptime = cputime;
+
+  fprintf('Temps de calcul %6.2f s\n',temps_calcul);
 end % for maillages
-
-% Nombre de pas de temps
-% ----------------------
-Nb_temps = round(T/delta_t);
-
-% Debut du chronometre
-% ---------------
-% A l'aide de cputime
-
-% solution a t=0 (U0(x) = 0 et U1(x) = 0)
-% --------------
-% UU0 = zeros(Nbpt,1);
-% UU1 = ....;
-
-% Boucle sur les pas de temps
-% ---------------------------
-% Uk-1 = UU0, Uk = UU1 et Uk+1 = UU2
-for k = 1:Nb_temps
-
-    tk = k*delta_t;
-        
-    % Calcul du second membre a l'instant tk
-    % --------------------------------------
-    % 1. Contribution de la source
-    % 2. Contribution des iteres precedents
-    % .
-    % A COMPLETER
-    % .
-
-    % Obtention de Uk+1
-    % -----------------
-    % .
-    % A COMPLETER
-    % .
-        
-    % Visualisation 
-    % -------------
-    affiche(UU2, Numtri, Coorneu, ['Temps = ', num2str(tk)]);
-    axis([min(Coorneu(:,1)),max(Coorneu(:,1)),min(Coorneu(:,2)),...
-          max(Coorneu(:,2)),-0.0002 0.0003  -0.00001 0.00015]);
-    % Mise a jour des iteres
-    % ----------------------
-    UU0 = UU1;
-    UU1 = UU2;    
-end
-
-% Arret du chronometre
-% --------------------
-% A l'aide de cputime
-% temps_calcul = ....
-
-fprintf('Temps de calcul %6.2f s\n',temps_calcul);
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                                                        fin de la routine
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
